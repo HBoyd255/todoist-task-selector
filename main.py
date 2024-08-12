72166298580a9f515eb239fda860c3fbef8ba05d
Harry Boyd <hboyd255@gmail.com>
2024-08-12 17:41:35 +0100

    Disabled debug mode

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ from modules.file_utils import text_file_to_string, save_object, load_object
 from modules.gui import GUI
 
 
-
-
 # If set to True, the data will be loaded from the pickle files.
 # If set to False, the data will be loaded from the Todoist API.
 # This allows for faster testing.
-LOAD_FROM_PICKLE = True
+LOAD_FROM_PICKLE = False
 
 # The API key is stored in a text file to keep it secret from the git
 # repository.
@@ -32,10 +30,8 @@ else:
     save_object(labels, "secrets/labels.pickle")
 
 
-
-
 label_names = [label.name for label in labels]
 
-gui = GUI(label_names,all_tasks)
+gui = GUI(label_names, all_tasks)
 
 gui.run()
